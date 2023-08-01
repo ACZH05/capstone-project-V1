217087346038b473da2b12a2a08ca96a1fa44ae1
Alfred Chin <alfredchin2005@gmail.com>
2023-08-01 18:55:22 +0800

    Add google login

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,7 @@
 import { Col, Row, Container, Button, Form } from "react-bootstrap";
 import '../style/AuthPage.css'
 import { useContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 import { AuthContext } from "../components/AuthProvider";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -18,9 +18,20 @@ export default function AuthPage() {
     const { currentUser } = useContext(AuthContext)
     const url = import.meta.env.VITE_BASE_URL
 
+    const provider = new GoogleAuthProvider()
+
     useEffect(() => {
-        if (currentUser) navigate("/")
+        if (currentUser) navigate("/login")
     }, [currentUser, navigate])
+
+    const handleGoogleLogin = async () => {
+        try {
+            signInWithPopup(auth, provider)
+        }
+        catch (error) {
+            console.log(error.code)
+        }
+    }
 
     const handleSignUp = async (e) => {
         e.preventDefault()
@@ -71,27 +82,29 @@ export default function AuthPage() {
     }
   return (
     <div style={{ backgroundColor: "#FAF3E9", height: "100dvh", overflow: "hidden"}}>
-        <Row className="flex-center" style={{ minWidth:"300px"}}>
+        <Row className="flex-center" style={{ minWidth:"300px", height: "70%"}}>
             <Col className="p-0 d-inline-flex mt-3 mx-3">
                 {active ? (
-                    <Container className="px-5 pt-4" style={{ width: "100%"}}>
+                    <Container className="px-3 pt-4" style={{ width: "100%"}}>
                         <div style={{ fontSize: 32, fontWeight: "bold", color: "#FF7F50"}}>Sign Up</div>
                         <Form onSubmit={handleSignUp}>
                             <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="mt-5" style={{ width: "100%"}} placeholder="Enter Email" required />
                             <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="mt-4" style={{ width: "100%"}} placeholder="Enter password" required />
                             <Button type="submit" className="mt-5 border-0 sign-up-button rounded-pill" style={{ width: "100%", backgroundColor: "#FF7F50"}}>Sign Up</Button>
-                            <p className="mt-3"><a href="" onClick={handleClickSignUp}>Got an account? Click here to sign in!</a></p>
+                            <p className="mt-3 text-center"><a href="" onClick={handleClickSignUp}>Got an account? Click here to sign in!</a></p>
                         </Form>
                     </Container>
                 ) : (
-                    <Container className="px-5 pt-4">
+                    <Container className="px-3 pt-4">
                         <div style={{ fontSize: 32, fontWeight: "bold", color: "#FF7F50"}}>Sign In</div>
                         <Form onSubmit={handleSignIn}>
                             <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)}  className="mt-5" style={{ width: "100%"}} placeholder="Enter Email" required />
                             <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="mt-4" style={{ width: "100%"}} placeholder="Enter password" required />
                             <Button type="submit" className="mt-5 border-0 sign-up-button rounded-pill" style={{ width: "100%", backgroundColor: "#FF7F50"}}>Sign In</Button>
+                            <hr />
+                            <Button variant="outline-dark" className="rounded-pill" style={{ width: "100%"}} onClick={handleGoogleLogin}><i className="bi bi-google me-2"></i>continue with google</Button>
                         </Form>
-                        <p className="mt-3"><a href="" onClick={handleClickSignIn}>First time? Click here to sign up!</a></p>
+                        <p className="mt-3 text-center"><a href="" onClick={handleClickSignIn}>First time? Click here to sign up!</a></p>
                     </Container>
 
                 )}
@@ -99,11 +112,11 @@ export default function AuthPage() {
                     <p className="text-danger">{error}</p>
                 )}
             </Col>
-            <Col className="p-0 row-cols-1 position-relative d-none d-md-inline-flex" style={{color: "#FAF3E9", backgroundColor: "#FF7F50", borderRadius: "0 20px 20px 0"}}>
+            <Col className="p-0 row-cols-1 position-relative d-none d-xl-inline-flex" style={{color: "#FAF3E9", backgroundColor: "#FF7F50", borderRadius: "0 20px 20px 0"}}>
                 <Container className="" style={{ width: "100%"}}>
                         <div className="position-absolute top-50 start-50 translate-middle" style={{color: "#FAF3E9"}}>
                         <h3>Welcome To</h3>
-                        <h1>EzBook</h1>
+                        <h1 className="text-center">EzBook</h1>
                     </div>
                     </Container>
             </Col>
